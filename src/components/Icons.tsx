56b1c9935a965db9620916b3d858a50395dd2669
brightybaron <ardi.panda54@protonmail.com>
2025-07-18 15:40:44 +0700

    push pertama

---
+++ src/components/Icons.tsx
@@ -0,0 +1,402 @@
+export const IconMapPinned = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5 mr-1 text-deep-blue"
+  >
+    <>
+      <path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0" />
+      <circle cx="12" cy="8" r="2" />
+      <path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712" />
+    </>
+  </svg>
+);
+
+export const IconMapPin = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-4 h-4 mr-1"
+  >
+    <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"></path>
+    <circle cx="12" cy="10" r="3"></circle>
+  </svg>
+);
+
+export const IconClock4 = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-4 h-4"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <polyline points="12 6 12 12 16 14"></polyline>
+  </svg>
+);
+
+export const IconPerson = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-4 h-4"
+  >
+    <>
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </>
+  </svg>
+);
+
+export const IconPeople = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-4 h-4"
+  >
+    <>
+      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+      <circle cx="9" cy="7" r="4" />
+      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+      <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+    </>
+  </svg>
+);
+
+export const IconChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="m18 15-6-6-6 6" />
+  </svg>
+);
+
+export const IconChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="m6 9 6 6 6-6" />
+  </svg>
+);
+
+export const IconChevronLeft = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="4"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <polyline points="15 18 9 12 15 6" />
+  </svg>
+);
+
+export const IconChevronRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="4"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <polyline points="9 18 15 12 9 6" />
+  </svg>
+);
+
+export const IconChat = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className || "w-5 h-5"}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z" />
+  </svg>
+);
+
+export const IconHome = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+  </svg>
+);
+
+export const IconPaperPlane = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
+    <path d="m21.854 2.147-10.94 10.939" />
+  </svg>
+);
+
+export const IconFileUser = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+    <path d="M15 18a3 3 0 1 0-6 0" />
+    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z" />
+    <circle cx="12" cy="13" r="2" />
+  </svg>
+);
+
+export const IconMapPinHouse = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M15 22a1 1 0 0 1-1-1v-4a1 1 0 0 1 .445-.832l3-2a1 1 0 0 1 1.11 0l3 2A1 1 0 0 1 22 17v4a1 1 0 0 1-1 1z"></path>
+    <path d="M18 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 .601.2"></path>
+    <path d="M18 22v-3"></path>
+    <circle cx="10" cy="10" r="3"></circle>
+  </svg>
+);
+
+export const IconMail = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+  </svg>
+);
+
+export const IconHamburger = () => (
+  <svg
+    className="h-6 w-6"
+    stroke="currentColor"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
+
+export const IconCross = () => (
+  <svg
+    className="h-6 w-6"
+    stroke="currentColor"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+);
+
+export const IconInstagram = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+    <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+  </svg>
+);
+
+export const IconFacebook = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+  </svg>
+);
+
+export const IconShoppingCart = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5"
+  >
+    <circle cx="8" cy="21" r="1" />
+    <circle cx="19" cy="21" r="1" />
+    <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+  </svg>
+);
+
+export const IconArrowRightUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M7 7h10v10" />
+    <path d="M7 17 17 7" />
+  </svg>
+);
+
+export const Icons = {
+  IconMapPinned,
+  IconMapPin,
+  IconClock4,
+  IconPerson,
+  IconPeople,
+  IconChevronDown,
+  IconChevronUp,
+  IconChevronLeft,
+  IconChevronRight,
+  IconChat,
+  IconHome,
+  IconPaperPlane,
+  IconFileUser,
+  IconMapPinHouse,
+  IconMail,
+  IconHamburger,
+  IconCross,
+  IconInstagram,
+  IconFacebook,
+  IconShoppingCart,
+  IconArrowRightUp,
+};
+
+export default Icons;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,427 @@
+import { useState, useEffect, useRef } from "react";
+import {
+  IconArrowRightUp,
+  IconChevronDown,
+  IconCross,
+  IconHamburger,
+  IconShoppingCart,
+} from "./Icons";
+
+const Navbar = ({ currentPath }: { currentPath: string }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isMobileDropdownOpen, setIsMobileDropdownOpen] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(false);
+
+  const prevPathRef = useRef(currentPath);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const mobileMenuRef = useRef<HTMLDivElement | null>(null);
+  const hoverTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  type MenuItem = {
+    page: string;
+    path: string;
+    hasDropdown?: boolean;
+    dropdownItems?: {
+      name: string;
+      img: string;
+      path: string;
+    }[];
+  };
+
+  const listMenu: MenuItem[] = [
+    { page: "About Us", path: "/about" },
+    {
+      page: "Shop",
+      path: "/shop",
+      hasDropdown: true,
+      dropdownItems: [
+        { name: "Meja", img: "/images/kursi-1.jpg", path: "/shop/meja" },
+        { name: "Kursi", img: "/images/kursi-2.jpg", path: "/shop/kursi" },
+        { name: "Lemari", img: "/images/sofa-1.jpg", path: "/shop/lemari" },
+        { name: "Rak", img: "/images/sofa-2.jpg", path: "/shop/rak" },
+        { name: "Sofa", img: "/images/sofa-3.jpg", path: "/shop/sofa" },
+        { name: "Lainnya", img: "", path: "/shop" },
+      ],
+    },
+    { page: "Blog", path: "/blog" },
+    { page: "FAQ", path: "/faq" },
+  ];
+
+  const isActive = (path: string) => {
+    if (path === "/" && currentPath === "/") return true;
+    if (path !== "/" && currentPath.startsWith(path)) {
+      if (path === "/shop" && currentPath !== "/shop") {
+        const dropdownItem = listMenu
+          .find((item) => item.path === "/shop")
+          ?.dropdownItems?.find((item) => item.path === currentPath);
+        return !dropdownItem;
+      }
+      return true;
+    }
+    return false;
+  };
+
+  // Close mobile menu when clicking on links
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+    setIsMobileDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    if (currentPath !== prevPathRef.current) {
+      setIsDropDownOpen(false);
+      setIsMobileDropdownOpen(false);
+      prevPathRef.current = currentPath;
+    }
+  }, [currentPath]);
+
+  // Handle hover for desktop dropdown
+  const handleMouseEnter = () => {
+    if (hoverTimeoutRef.current) {
+      clearTimeout(hoverTimeoutRef.current);
+    }
+    setIsDropDownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    hoverTimeoutRef.current = setTimeout(() => {
+      setIsDropDownOpen(false);
+    }, 150); // Small delay to prevent flickering
+  };
+
+  const handleMobileMenuClick = () => {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+    isMenuOpen ? setIsMobileDropdownOpen(false) : setIsMobileDropdownOpen(true);
+  };
+
+  // Disable body scroll when mobile menu is open
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isMenuOpen]);
+
+  const toggleMobileDropdown = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsMobileDropdownOpen(!isMobileDropdownOpen);
+  };
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position > 80);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Cleanup timeout on unmount
+  useEffect(() => {
+    return () => {
+      if (hoverTimeoutRef.current) {
+        clearTimeout(hoverTimeoutRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <nav
+        className={`fixed top-0 left-1/2 transform -translate-x-1/2 z-50 bg-white md:bg-transparent transition-all ease-linear duration-300 mt-2 md:mt-0 w-[95%] rounded-full md:rounded-none ${
+          scrollPosition ? "md:w-[90%]" : "md:w-full"
+        }`}
+        aria-label="Navigation"
+      >
+        <div
+          className={`hidden md:relative md:flex justify-center items-center w-full bg-stone-300 font-semibold text-gray-600 transition-all duration-300 ease-in-out${
+            scrollPosition
+              ? "-translate-y-[150%] opacity-0 py-0"
+              : "translate-y-0 opacity-100 py-2"
+          }`}
+        >
+          <a
+            href="#"
+            className="relative flex justify-center items-center w-full h-full hover:underline hover:underline-offset-2 group"
+          >
+            <div>
+              Gerai kami di Jl. Jawa No.2 GKB buka setiap hari pukul 10:00 -
+              19:00
+            </div>
+            <div className="absolute flex left-auto right-6 justify-start items-center">
+              <div>Pusat bantuan</div>
+              <span className="group-hover:rotate-45 transition-all duration-200 ease-in-out">
+                <IconArrowRightUp />
+              </span>
+            </div>
+          </a>
+        </div>
+        <div className="relative w-full mx-auto">
+          <div
+            className={`flex justify-between items-center min-h-16 transition-all duration-200 ease-in-out px-2 ${
+              scrollPosition
+                ? "md:bg-white md:rounded-t-full md:rounded-b-full md:py-2 md:px-2"
+                : "md:py-2 md:px-12"
+            } ${
+              isDropDownOpen
+                ? scrollPosition
+                  ? "md:rounded-t-xl md:rounded-b-none"
+                  : "md:bg-white"
+                : ""
+            }`}
+          >
+            {/* Desktop Menu */}
+            <div className="flex">
+              {/* Logo */}
+              <a href="/" className="flex-shrink-0 flex items-center gap-x-1">
+                <img
+                  src="/logo.png"
+                  alt="Logo"
+                  className="h-12 w-12 md:w-16 md:h-16"
+                />
+              </a>
+
+              {/* Menu */}
+              <div className="hidden md:flex ml-12 items-center space-x-4">
+                {listMenu.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`px-3 py-2 font-medium text-lg after:content-[''] after:block after:border-b-2 after:border-black after:transition-all after:duration-300 ${
+                      isActive(item.path)
+                        ? "after:scale-x-100"
+                        : "after:scale-x-0 after:origin-center"
+                    }`}
+                  >
+                    {item.hasDropdown ? (
+                      <div
+                        ref={dropdownRef}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        className="relative"
+                      >
+                        <div className="inline-flex items-center gap-x-1 transition-all duration-300 transform hover:cursor-pointer">
+                          {item.page}
+                          <div
+                            className={`transition-transform duration-300 ${
+                              isDropDownOpen ? "rotate-180" : "rotate-0"
+                            }`}
+                          >
+                            <IconChevronDown />
+                          </div>
+                        </div>
+                        {/* {isDropDownOpen && (
+                          <div className="absolute w-48 bg-white rounded-lg shadow-lg border z-30">
+                            <div className="p-2">
+                              {item.dropdownItems?.map((dropdownItem, idx) => (
+                                <a
+                                  key={idx}
+                                  href={dropdownItem.path}
+                                  className={`block px-4 py-2 rounded-md transition-colors ${
+                                    isDropdownItemActive(dropdownItem.path)
+                                      ? "text-black font-bold bg-gray-100"
+                                      : "text-gray-700 hover:bg-gray-100"
+                                  }`}
+                                >
+                                  {dropdownItem.name}
+                                </a>
+                              ))}
+                            </div>
+                          </div>
+                        )} */}
+                      </div>
+                    ) : (
+                      <a href={item.path}>{item.page}</a>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex items-center gap-x-4">
+              {/* Shopping Cart */}
+              <div className="relative">
+                <button className="bg-stone-400 rounded-full flex items-center justify-center hover:bg-stone-300 hover:cursor-pointer transition-all duration-300 w-12 h-12">
+                  <IconShoppingCart />
+                </button>
+
+                {/* Badge */}
+                <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                  3
+                </div>
+              </div>
+
+              {/* Mobile menu button */}
+              <button
+                id="mobile-menu-button"
+                onClick={handleMobileMenuClick}
+                className="md:hidden p-2 z-50 relative ring-1 ring-stone-400 rounded-full"
+              >
+                {isMenuOpen ? <IconCross /> : <IconHamburger />}
+              </button>
+            </div>
+          </div>
+
+          {/* Showing Dropdown item when hover */}
+          {listMenu.find((item) => item.hasDropdown) && (
+            <div
+              ref={dropdownRef}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={`relative top-0 bg-white transition-all duration-300 ease-in-out transform overflow-hidden py-4 rounded-b-xl hidden md:block ${
+                isDropDownOpen
+                  ? `translate-y-0 opacity-100 h-auto w-full ${
+                      scrollPosition ? "px-2" : "px-12"
+                    }`
+                  : "-translate-y-full opacity-0 h-0 w-0"
+              }`}
+            >
+              <div className="grid grid-cols-6 gap-4 px-2">
+                {listMenu
+                  .find((item) => item.hasDropdown)
+                  ?.dropdownItems?.map((dropdownItem, idx) => (
+                    <a key={idx} href={dropdownItem.path}>
+                      <div
+                        className={`aspect-square flex items-center justify-center overflow-hidden bg-gray-200 group w-48`}
+                      >
+                        {dropdownItem.img ? (
+                          <img
+                            src={dropdownItem.img}
+                            alt={
+                              dropdownItem.img === null ? dropdownItem.name : ""
+                            }
+                            className="h-full w-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+                          />
+                        ) : (
+                          <span className="text-4xl text-gray-500 relative">
+                            <span className="absolute inset-0 flex items-center justify-center transition-all duration-300 group-hover:rotate-45 group-hover:scale-110">
+                              +
+                            </span>
+                          </span>
+                        )}
+                      </div>
+                      <p className="mt-2 font-medium">{dropdownItem.name}</p>
+                    </a>
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+
+      {/* Mobile Menu Fullscreen */}
+      <div
+        id="mobile-menu"
+        ref={mobileMenuRef}
+        className={`md:hidden fixed inset-0 bg-white z-40 flex flex-col transition-transform duration-500 ease-linear ${
+          isMenuOpen
+            ? "translate-y-0"
+            : "-translate-y-[100%] pointer-events-none"
+        }`}
+      >
+        {/* Header space to account for navbar */}
+        <div className="h-20"></div>
+
+        {/* Menu Items */}
+        <div className="flex-1 overflow-y-auto px-6 py-4">
+          {listMenu.map((item, index) => (
+            <div
+              key={index}
+              className="border-b-2 border-stone-300 last:border-b-0"
+            >
+              {item.hasDropdown ? (
+                <div>
+                  <button
+                    onClick={toggleMobileDropdown}
+                    className={`w-full flex items-center justify-between py-4 text-left font-medium text-lg ${
+                      isActive(item.path) ? "text-blue-600" : "text-gray-900"
+                    }`}
+                  >
+                    <span>{item.page}</span>
+                    <span
+                      className={`transition-all duration-300 ease-in-out ${
+                        isMobileDropdownOpen ? "rotate-180" : ""
+                      }`}
+                    >
+                      <IconChevronDown />
+                    </span>
+                  </button>
+                  {isMobileDropdownOpen && (
+                    <div className="pb-4 flex gap-4 overflow-scroll">
+                      {listMenu
+                        .find((item) => item.hasDropdown)
+                        ?.dropdownItems?.map((dropdownItem, idx) => (
+                          <a key={idx} href={dropdownItem.path}>
+                            <div
+                              className={`aspect-square flex items-center justify-center overflow-hidden bg-gray-200 group w-32`}
+                            >
+                              {dropdownItem.img ? (
+                                <img
+                                  loading="eager"
+                                  src={dropdownItem.img}
+                                  alt={
+                                    dropdownItem.img === null
+                                      ? dropdownItem.name
+                                      : ""
+                                  }
+                                  className="h-full w-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+                                />
+                              ) : (
+                                <span className="text-4xl text-gray-500 relative">
+                                  <span className="absolute inset-0 flex items-center justify-center transition-all duration-300 group-hover:rotate-45 group-hover:scale-110">
+                                    +
+                                  </span>
+                                </span>
+                              )}
+                            </div>
+                            <p className="mt-2 font-medium">
+                              {dropdownItem.name}
+                            </p>
+                          </a>
+                        ))}
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <a
+                  href={item.path}
+                  onClick={handleLinkClick}
+                  className={`block py-2 text-lg font-semibold ${
+                    isActive(item.path) ? "text-blue-600" : "text-gray-900"
+                  }`}
+                >
+                  {item.page}
+                </a>
+              )}
+            </div>
+          ))}
+        </div>
+
+        {/* Footer section */}
+        <div className="border-t border-gray-200 p-6">
+          <div className="text-center text-gray-600 text-sm">
+            <p>Our showroom in GKB is open today</p>
+            <p>08:00 - 20:00</p>
+            <a
+              href="#"
+              className="inline-flex items-center gap-x-1 mt-2 text-blue-600 hover:underline"
+            >
+              Pusat bantuan
+              <IconArrowRightUp />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
